2ee8d816397ebbafe166ac0466915e648abcf96e
carol11 <car.hanna123@gmail.com>
2014-02-08 20:39:15 -0500

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Carol11DemoApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root '/'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
